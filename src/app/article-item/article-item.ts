0a75a932f4837248c71f540f0fb8ed5b527dc940
Miki <miquelxubet@gmail.com>
2026-05-03 14:56:15 +0200

    Exercici3 - Implementació NgClass i NgIf

--- src/app/article-item/article-item.ts
+++ src/app/article-item/article-item.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Article } from '../models/article';
 
 @Component({
   selector: 'app-article-item',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './article-item.html',
   styleUrl: './article-item.css',
 })
